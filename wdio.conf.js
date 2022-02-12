41c17fb94021690ba6dd4cb696226d78aead5d84
Redinbox <99498502+Redinbox@users.noreply.github.com>
2022-02-12 14:13:42 +0530

    Create wdio.conf.js

---
+++ wdio.conf.js
@@ -0,0 +1,101 @@
+const path = require('path');
+
+exports.config = {
+    //
+    // ====================
+    // Runner Configuration
+    // ====================
+    //
+    // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
+    // on a remote machine).
+    runner: 'local',
+    debug:true,
+    path: '/wd/hub',
+    //
+    // ==================
+    // Specify Test Files
+    // ==================
+    specs: ['./test/*.js'],
+    // Patterns to exclude.
+    exclude: [
+        // 'path/to/excluded/files'
+    ],
+    //
+    // ============
+    // Capabilities
+    // ============
+
+    maxInstances: 10,
+    //
+    // If you have trouble getting all important capabilities together, check out the
+    // Sauce Labs platform configurator - a great tool to configure your capabilities:
+    // https://docs.saucelabs.com/reference/platforms-configurator
+    //
+    capabilities: [
+        {
+            // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+            // grid with only 5 firefox instances available you can make sure that not more than
+            // 5 instances get started at a time.
+            maxInstances: 5,
+            browserName: 'chrome',
+            acceptInsecureCerts: true,
+            'selenoid:options': {
+                enableVnc: true,
+            },
+        },
+        {
+            maxInstances: 1,
+            //
+            browserName: 'firefox',
+            'selenoid:options': {
+                enableVnc: true,
+            },
+        },
+    ],
+    //
+    // ===================
+    // Test Configurations
+    // ===================
+    // Define all options that are relevant for the WebdriverIO instance here
+    //
+    // Level of logging verbosity: trace | debug | info | warn | error | silent
+    logLevel: 'trace',
+
+    // bail (default is 0 - don't bail, run all tests).
+    bail: 0,
+
+    baseUrl: 'https://webdriver.io',
+    //
+    // Default timeout for all waitFor* commands.
+    waitforTimeout: 10000,
+    //
+    // Default timeout in milliseconds for request
+    // if browser driver or grid doesn't send response
+    connectionRetryTimeout: 120000,
+    //
+    // Default request retries count
+    connectionRetryCount: 3,
+
+    services: [
+       [
+            path.join(process.cwd(), 'dist', 'index.js'),
+        ],
+    ],
+
+    reporters: ['spec'],
+
+    //
+    // Options to be passed to Jasmine.
+    jasmineOpts: {
+        // Jasmine default timeout
+        defaultTimeoutInterval: 60000,
+        //
+        // The Jasmine framework allows interception of each assertion in order to log the state of the application
+        // or website depending on the result. For example, it is pretty handy to take a screenshot every time
+        // an assertion fails.
+        expectationResultHandler: function (passed, assertion) {
+            // do something
+        },
+    },
+
+};
